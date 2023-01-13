8a1bb231e02d87f96e36f3329c3f7d1d7f001661
Umesh Kumar Chaurasiya <113828943+Anik273155@users.noreply.github.com>
2023-01-13 16:26:53 +0530

    changes in first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React ,{Component,useState} from "react";
+
+import './App.css';
+
+const  App=()=>{
+
+  return (
+    <div id="main">
+    <p>I am learning React. My life is getting better.</p>
+       <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
